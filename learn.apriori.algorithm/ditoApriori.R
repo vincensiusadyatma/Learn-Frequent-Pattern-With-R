21e44ef45478248aef029d921ce35f7296adcb6f
KrishnanditoLksn <krislaksana123@gmail.com>
2025-03-15 21:01:21 +0700

    feat:added apriori rule implementation in custom dataset

---
+++ learn.apriori.algorithm/ditoApriori.R
@@ -0,0 +1,41 @@
+install.packages("arules")
+library(arules)
+
+# Cek working directory
+getwd()
+
+# Baca data transaksi
+data_trans1 <- read.transactions(
+  file = 'D://Projects/r_apriori/Learn-Frequent-Pattern-With-R/learn.apriori.algorithm/items.csv',
+  sep = ","
+)
+
+# Periksa transaksi yang telah dimuat
+inspect(data_trans1)
+
+# Menampilkan daftar item
+data_trans1@itemInfo
+
+# Menampilkan Data
+data_trans1@data
+
+# Menampilkan Item frequency
+itemFrequency(data_trans1, type="absolute")
+
+# Simpan dan urutkan item frequency dalam bentuk absolute
+data1_item <- sort(itemFrequency(data_trans1, type="absolute"), decreasing = TRUE)
+data1_item
+
+# Tampilkan tiga item paling sering muncul
+data1_item[1:3]
+
+# Konversi ke transaksi
+trans <- as(data_trans1, "transactions")
+
+# Gunakan nilai support yang lebih rendah agar hasil tidak NULL
+freq1 <- apriori(data_trans1, parameter=list(support=0.5, target="frequent itemsets"))
+
+inspect(freq1)
+
+aturan <- apriori(data_trans1)
+inspect(aturan)
